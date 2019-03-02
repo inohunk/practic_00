2512ef9db5d28dbb8d33d546b0f60b7fa0248892
hunkel <kolan19988@yandex.ru>
2019-03-02 12:56:03 +0300

    Доработан алгоритм добавления свойств символьному объекту

--- src/ocad_package/ocad_versions/OCADAbstract.java
+++ src/ocad_package/ocad_versions/OCADAbstract.java
@@ -1,7 +1,5 @@
 package ocad_package.ocad_versions;
 
-import ocad_package.interfaces.IOCADCreator;
-import ocad_package.interfaces.SymbolManager;
 import ocad_package.symbols.*;
 
 import java.nio.ByteBuffer;
@@ -12,15 +10,19 @@ import java.util.ArrayList;
  * @version 0.1.0
  * Abstract OCAD Class
  */
-public abstract class OCADAbstract implements IOCADCreator {
+public abstract class OCADAbstract{
 
     protected ByteBuffer buffer = null;
     public OCADAbstract(ByteBuffer buf)
     {
         this.buffer = buf;
+        getNumberOfCharacters();
     }
 
+
     //File Header
+    public int getOcadMark() { return buffer.getShort(0);}
+
     public int getFileType(){ return buffer.get(2);}
 
     public int getFileStatus() { return buffer.get(3);}
@@ -37,6 +39,178 @@ public abstract class OCADAbstract implements IOCADCreator {
 
     public int getFirstObjectBlock() { return buffer.getInt(12);}
 
+    public int getOfflineSyncSerial() { return  buffer.getInt(16);}
+
+    public int getCurrentFileVersion() { return buffer.getInt(20);}
+
+    public int getFirstStringIndexBlock() { return buffer.getInt(32);}
+
+    public int getFileNamePosition()
+    {
+        return buffer.getInt(36);
+    }
+
+    public int getFileNameSize() { return buffer.getInt(40); }
+
+
+    //Symbols
+    private ArrayList<BaseSymbol> symbols = new ArrayList<>();
+
+    private int getSymbolSize(int index) { return buffer.getInt(index);}
+    private int getSymbolNumber(int index) { return buffer.getInt(index+4);}
+    private int getSymbolOTP(int index) { return buffer.get(index+8);}
+    private int getSymbolFlags(int index) { return buffer.get(index+9);}
+    private boolean getSymbolSelected(int index)
+    {
+        byte var = buffer.get(index+10);
+
+        return true && var != 0;
+    }
+    private int getSymbolStatus(int index) { return buffer.get(index+11);}
+    private int getSymbolPreferredDrawingTool(int index) { return buffer.get(index+12);}
+    private int getCsMode(int index) {return buffer.get(index+13);}
+    private int getCsObjectType(int index) {return buffer.get(index+14);}
+    private int getCsCdFlags(int index) { return buffer.get(index+15);}
+    private int getExtent(int index) { return buffer.getInt(index+16);}
+    private int getFilePosition(int index) { return buffer.get(index+20);}
+    private int getNumberOfColors(int index) { return buffer.getInt(index+26);}
+
+    public ArrayList<BaseSymbol> getSymbols() {return symbols;}
+    private int counter = 0;
+    private void addDataOfSymbols(int startIndex)
+    {
+        counter++;
+        BaseSymbol sym = symbolCreator(getSymbolOTP(startIndex));
+        //sym.setParameters(getSymbolSize(startIndex),getSymbolNumber(startIndex),getSymbolOTP(startIndex),getSymbolFlags(startIndex));
+        sym.setSize(getSymbolSize(startIndex));
+        sym.setNumber(getSymbolNumber(startIndex));
+        sym.setOtp((byte) getSymbolOTP(startIndex));
+        sym.setFlags((byte) getSymbolFlags(startIndex));
+        //sym.setSelected(getSymbolSelected(startIndex));
+        sym.setStatus((byte) getSymbolStatus(startIndex));
+        sym.setPrefferedDrawingTool((byte) getSymbolPreferredDrawingTool(startIndex));
+        sym.setCsMode((byte) getCsMode(startIndex));
+        sym.setCsObjectType((byte) getCsObjectType(startIndex));
+        sym.setCsCdFlags((byte) getCsCdFlags(startIndex));
+        sym.setExtend(getExtent(startIndex));
+        sym.setFilePos(getFilePosition(startIndex));
+        sym.setnColors((short) getNumberOfColors(startIndex));
+        System.out.println("=========" + counter + "=========");
+        System.out.println("Size: "+sym.getSize());
+        System.out.println("Number: "+sym.getNumber());
+        System.out.println("OTP: "+sym.getOtp());
+        System.out.println("Flags: "+sym.getFlags());
+        System.out.println("Status: "+sym.getStatus());
+        System.out.println("PrefferedDrawing: "+sym.getPrefferedDrawingTool());
+        System.out.println("CsMode: "+sym.getCsMode());
+        System.out.println("CsObjectType: "+sym.getCsObjectType());
+        System.out.println("CsCdFlags: "+sym.getCsCdFlags());
+        System.out.println("Extend: "+sym.getExtend());
+        System.out.println("FilePos: "+sym.getFilePos());
+        System.out.println("Number of Colors: "+sym.getnColors());
+
+        symbols.add(sym);
+
+    }
+
+    public int getNumberOfCharacters()
+    {
+
+        int index = getFirstSymbolIndexBlock()+4;
+        int nextSym = getFirstSymbolIndexBlock();
+        int count=0;
+        boolean active = true;
+        while(active)
+        {
+            if(buffer.getInt(nextSym)!=0) {
+                nextSym = buffer.getInt(nextSym);
+                int i = 0;
+                while (i++ < 256)
+                {
+                    addDataOfSymbols(buffer.getInt(index));
+
+                    //Integer indexValue = getSymbolID(index);
+                    //symbols.put(index,indexValue);
+                    count++;
+                    index+=4;
+                }
+            } else {
+                int i = 0;
+                index = nextSym + 4;
+                while (i++ < 256 && buffer.getInt(index)!=0)
+                {
+                    addDataOfSymbols(buffer.getInt(index));
+                    //Integer indexValue = getSymbolID(index);
+                    //symbols.put(index,indexValue);
+                    count++;
+                    index+=4;
+                }
+                active = false;
+            }
+        }
+        return count;
+    }
+    //public int getSymbolSize() {return }
+    public void outSymbols()
+    {
+        try {
+            for (int i = 0; i < symbols.size(); i++) {
+                System.out.println(symbols.size());
+                //SymbolManager baseSymbol = symbols.get(i);
+                //System.out.println(baseSymbol.getSymNum());
+            }
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static BaseSymbol symbolCreator(int otp)
+    {
+        BaseSymbol symbol = null;
+        try {
+
+            switch (otp) {
+                case 1:
+                    symbol = new PointSymbol();
+                    break;
+                case 2:
+                    symbol = new LineSymbol();
+                    break;
+
+                case 3:
+                    symbol = new AreaSymbol();
+                    break;
+                case 4:
+                    symbol = new TextSymbol();
+                    break;
+                case 6:
+                    symbol = new LineTextSymbol();
+                    break;
+                case 7:
+                    symbol = new RectangleSymbol();
+                    break;
+            }
+            //System.out.println("Symbol created");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return symbol;
+    }
+    byte getOTP(int startIndex)
+    {
+        return buffer.get(buffer.get(startIndex)+5);
+    }
+    byte getFlags(int startIndex)
+    {
+        return buffer.get(buffer.get(startIndex)+6);
+    }
+
+
+    //Other
 
     private String hexToString(String hex) {
         StringBuilder sb = new StringBuilder();
@@ -48,14 +222,6 @@ public abstract class OCADAbstract implements IOCADCreator {
         return sb.toString();
     }
 
-    public int getFileNamePosition()
-    {
-        return buffer.getInt(36);
-    }
-    public int getFileNameSize()
-    {
-        return buffer.getInt(40);
-    }
     public String getFileName()
     {
         String source = "";
@@ -76,32 +242,14 @@ public abstract class OCADAbstract implements IOCADCreator {
     }
     //Symbols
     //private Map<Integer,Integer> symbols = new HashMap<Integer, Integer>();
-    private ArrayList<BaseSymbol> symbols = new ArrayList<>();
 
-    public void outSymbols()
-    {
-        try {
-            for (int i = 0; i < symbols.size(); i++) {
-                System.out.println(symbols.size());
-                SymbolManager baseSymbol = symbols.get(i);
-                System.out.println(baseSymbol.getSymNum());
-            }
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
-    }
-    public int  getSymbolID(int index)
-    {
-        return buffer.getInt(buffer.getInt(index)+4);
-    }
+
 
     public int test() {
         return buffer.getInt(40);
     }
 
-    public int getNumberOfCharacters()
+    /*public int getNumberOfCharacters()
     {
         int nextBlock = getFirstSymbolIndexBlock(); //Ссылка на следующий массив символов
         int index = getFirstSymbolIndexBlock()+4; //Ссылка на первый символ
@@ -142,58 +290,9 @@ public abstract class OCADAbstract implements IOCADCreator {
             }
         }
         return count;
-    }
-    private static BaseSymbol symbolCreator(int otp)
-    {
-        BaseSymbol symbol = null;
-        try {
+    }*/
 
-            switch (otp) {
-                case 1:
-                    symbol = new PointSymbol();
-                    break;
-                case 2:
-                    symbol = new LineSymbol();
-                    break;
 
-                case 3:
-                    symbol = new AreaSymbol();
-                    break;
-                case 4:
-                    symbol = new TextSymbol();
-                    break;
-                case 6:
-                    symbol = new LineTextSymbol();
-                    break;
-                case 7:
-                    symbol = new RectangleSymbol();
-                    break;
-            }
-            System.out.println("Symbol created");
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        return symbol;
-    }
-    int getSymbolSize(int startIndex)
-    {
-        return buffer.getInt(buffer.getInt(startIndex));
-    }
-
-    int getSymbolNumber(int startIndex)
-    {
-        return buffer.getInt(buffer.getInt(startIndex)+4);
-    }
-    byte getOTP(int startIndex)
-    {
-        return buffer.get(buffer.get(startIndex)+5);
-    }
-    byte getFlags(int startIndex)
-    {
-        return buffer.get(buffer.get(startIndex)+6);
-    }
 
 
 }
